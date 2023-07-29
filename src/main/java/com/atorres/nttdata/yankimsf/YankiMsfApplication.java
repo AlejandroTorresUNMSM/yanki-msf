01ff446312b6314cf2785f56fa25293ccae77526
AlejandroTorresUNSMS <alephdmc2575@gmail.com>
2023-07-29 11:44:48 -0500

    dev: commit inicial

---
+++ src/main/java/com/atorres/nttdata/yankimsf/YankiMsfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.yankimsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YankiMsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YankiMsfApplication.class, args);
+	}
+
+}
